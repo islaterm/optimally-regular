47086db2358d276300c03c69c1b50383ea0a410f
SL8R <ignacio.slater@ug.uchile.cl>
2020-11-17 18:30:03 -0300

    Adam constructor

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,5 @@
 # Changelog
 
+- ``0.2.4.2`` Adam constructor
 - ``0.2.4.1`` Adam class declaration
 - ``0.2.4.0`` Instructions
--- src/optimization/Adam.py
+++ src/optimization/Adam.py
@@ -6,11 +6,24 @@ You should have received a copy of the license along with this
 work. If not, see <http://creativecommons.org/licenses/by/4.0/>.
 """
 
+from typing import Tuple
+from torch import Tensor
+
 
 class Adam():
     """Implementation of the Adam algorithm.
     """
-    def __init__(self, parameters, lr=0.001, beta1=0.9, beta2=0.999, epsilon=1e-8):
+    __learning_rate: float
+    __betas: Tuple[float, float]
+    __epsilon: float
+    __parameters: Tensor
+
+    def __init__(self,
+                 parameters,
+                 lr=0.001,
+                 beta1=0.9,
+                 beta2=0.999,
+                 epsilon=1e-8):
         """
         Implements the Adam algorithm as proposed by Kingma et.al. 
         (arXiv:1412.6980)
@@ -29,9 +42,12 @@ class Adam():
                 term added to the denominator to improve numerical stability; 
                 defaults to 1e-8
         """
-        pass
+        self.__learning_rate = lr
+        self.__betas = (beta1, beta2)
+        self.__epsilon = epsilon
+        self.__parameters = parameters
 
     def step(self):
-        # actualiza acá los parámetros a partir de los gradientes
-        # y la corrección según S
+        """Performs a single optimization step.
+        """
         pass
