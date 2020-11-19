87d21bfc82caaa05e00812f21653034cd7a5a374
SL8R <ignacio.slater@ug.uchile.cl>
2020-11-19 15:51:36 -0300

    Pre-merge commit

--- src/deep_learning/optimization/xavier_he.py
+++ src/deep_learning/optimization/xavier_he.py
@@ -3,6 +3,6 @@ from torch import Tensor
 from torch import Tensor
 from torch import nn
 
-nn.init.xavier_normal
+
 def xavier_he_init(weights_tensor: Tensor):
-  prev_d = weights_tensor.size()[1]
+    prev_d = weights_tensor.size()[1]
