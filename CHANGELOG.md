dbd27bd8e5021065ad30fef29384b84229816e8a
SL8R <ignacio.slater@ug.uchile.cl>
2020-11-19 21:17:38 -0300

    RMSProp step function
    Took 34 minutes

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,5 @@
 # Changelog
 
+- ``0.2.3.2`` RMSProp step function
 - ``0.2.3.1`` RMSprops class declaration
 - ``0.2.3.0`` Instructions
--- src/rms_prop.py
+++ src/rms_prop.py
@@ -6,17 +6,21 @@ You should have received a copy of the license along with this
 work. If not, see <http://creativecommons.org/licenses/by/4.0/>.
 """
 
-from typing import Iterable
+from typing import List
+
+import torch
+from torch import Tensor
 
 
 class RMSProp:
     """Root mean square propagation implementation.
     """
-
-    __lr: float
     __beta: float
     __epsilon: float
-    __parameters: Iterable
+    __lr: float
+    __parameters: List[Tensor]
+    __square_avg: Tensor
+    __step: int
 
     def __init__(self, parameters, lr=0.001, beta=0.9, epsilon=1e-8):
         """
@@ -25,13 +29,13 @@ class RMSProp:
         <https://www.cs.toronto.edu/~tijmen/csc321/slides/lecture_slides_lec6.pdf>.
 
         Args:
-            parameters (iterable):
+            parameters:
                 iterable of parameters to optimize
-            lr (float, optional):
+            lr:
                 the learning rate; defaults to 0.001
-            beta (float, optional):
+            beta:
                 the smoothing constant; defaults to 0.9
-            epsilon ([type], optional):
+            epsilon:
                 term added to the denominator to improve numerical stability; 
                 defaults to 1e-8
         """
@@ -39,8 +43,16 @@ class RMSProp:
         self.__beta = beta
         self.__epsilon = epsilon
         self.__parameters = parameters
+        self.__step = 0
+        self.__square_avg = torch.zeros_like(parameters)
 
     def step(self):
         """Performs a single optimization step."""
         for parameter in self.__parameters:
-            pass
+            if parameter.grad is not None:
+                grad = parameter.grad
+                self.__step += 1
+                self.__square_avg = torch.addcmul(torch.mul(self.__square_avg, self.__beta), grad,
+                                                  grad, value=1 - self.__beta)
+                avg = torch.sqrt(self.__square_avg) + self.__epsilon
+                torch.addcdiv(parameter, grad, avg, value=self.__lr, out=parameter)
